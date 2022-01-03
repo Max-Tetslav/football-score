98f48f6ca9a8d8cba5174b57ad789ec4490cd278
Max Teclav <maks.tetslav@yandex.ru>
2022-01-03 18:30:18 +0300

    feat: initial commit

---
+++ src/app/About.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+export default function About(){
+  return (
+    // <Fragment>
+        <div>
+        <h2>About Page</h2>
+        <main>
+          <p>This section contains information about...</p>
+        </main>
+      </div>
+    // </Fragment>
+   
+  )
+}
